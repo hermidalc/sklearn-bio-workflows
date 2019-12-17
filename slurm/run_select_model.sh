b224b1cfbff180ff06c52b23f571d3fd713cc58a
Leandro Hermida <hermidal@cs.umd.edu>
2019-12-17 16:41:02 -0500

    More updates to get slurm working

--- slurm/run_select_model.sh
+++ slurm/run_select_model.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname $(realpath -s $0))
+export MPLBACKEND=agg
+export TMPDIR=/lscratch/$SLURM_JOB_ID
 
-args=()
-get_n_jobs=false
-for (( i=1; i<=$#; i++ )); do
-    args+=(${!i})
-    if [[ ${!i} == "--n-jobs" ]]; then
-        get_n_jobs=true
-    elif [[ $get_n_jobs == true ]]; then
-        n_jobs=${!i}
-        get_n_jobs=false
-    fi
-done
-if [[ ! -v n_jobs ]]; then
-    n_jobs=64
-    args+=("--n-jobs" "$n_jobs")
-fi
-sbatch \
---chdir="$(realpath $SCRIPT_PATH/../)" \
---cpus-per-task=$n_jobs \
---mem="$((n_jobs*2))g" \
---partition=ccr,norm \
---time=48:00:00 \
-$SCRIPT_PATH/run_select_model_slurm.sh "${args[@]}"
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate sklearn-bio-workflows
+cmd=("$SLURM_SUBMIT_DIR/select_model.py" "$@")
+echo "${cmd[@]}"
+"${cmd[@]}"

fe796a3fc8ecfa031f3cbf38d4c2404a9930bb7c
Leandro Hermida <hermidal@cs.umd.edu>
2020-01-07 11:16:10 -0500

    Set lib num threads env vars in slurm script

--- slurm/run_select_model.sh
+++ slurm/run_select_model.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+export BLIS_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+export NUMBA_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+export VECLIB_MAXIMUM_THREADS=1
+
 export MPLBACKEND=agg
 export TMPDIR=/lscratch/$SLURM_JOB_ID
 

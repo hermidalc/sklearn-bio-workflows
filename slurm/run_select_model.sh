131eb0da806412ce89a3cf04c4ad778f5741d180
Leandro Hermida <hermidal@cs.umd.edu>
2020-02-10 10:27:06 -0500

    Slurm shorter env name update

--- slurm/run_select_model.sh
+++ slurm/run_select_model.sh
@@ -13,7 +13,7 @@ export TMPDIR=/lscratch/$SLURM_JOB_ID
 
 CONDA_BASE=$(conda info --base)
 source $CONDA_BASE/etc/profile.d/conda.sh
-conda activate sklearn-bio-workflows
+conda activate sklearn-workflows
 
 cmd=("$SLURM_SUBMIT_DIR/select_model.py" "$@")
 echo "${cmd[@]}"

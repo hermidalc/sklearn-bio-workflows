4fd65c5e47163bdeeb3de9e6a47d2c7abf63dee7
Leandro Hermida <hermidal@cs.umd.edu>
2020-06-18 13:56:56 -0400

    Additional feature metadata and penalty factor support for new RFE

--- select_model.py
+++ select_model.py
@@ -654,6 +654,37 @@ def run_model_selection():
      col_trf_columns) = load_dataset(args.train_dataset)
     pipe, pipe_name, pipe_props, param_grid, param_grid_dict, _ = (
         setup_pipe_and_param_grid(args.pipe_steps, col_trf_columns))
+    if args.sample_meta_cols:
+        has_penalty_factor = False
+        if hasattr(pipe[-1], 'penalty_factor_meta_col'):
+            pipe[-1].set_params(
+                penalty_factor_meta_col=args.penalty_factor_meta_col)
+            has_penalty_factor = True
+        elif (hasattr(pipe[-1], 'estimator')
+              and hasattr(pipe[-1].estimator, 'penalty_factor_meta_col')):
+            pipe[-1].estimator.set_params(
+                penalty_factor_meta_col=args.penalty_factor_meta_col)
+            has_penalty_factor = True
+        elif pipe[-1] is None:
+            srv_step_name = pipe.steps[-1][0]
+            for params in param_grid:
+                if srv_step_name in params:
+                    if (hasattr(params[srv_step_name][0],
+                                'penalty_factor_meta_col')):
+                        params[srv_step_name][0].set_params(
+                            penalty_factor_meta_col=(
+                                args.penalty_factor_meta_col))
+                        has_penalty_factor = True
+                    elif (hasattr(params[srv_step_name][0], 'estimator')
+                          and hasattr(params[srv_step_name][0].estimator,
+                                      'penalty_factor_meta_col')):
+                        params[srv_step_name][0].estimator.set_params(
+                            penalty_factor_meta_col=(
+                                args.penalty_factor_meta_col))
+                        has_penalty_factor = True
+        if not has_penalty_factor:
+            feature_meta.drop(columns=[args.penalty_factor_meta_col],
+                              inplace=True)
     search_param_routing = ({'cv': 'groups', 'estimator': [], 'scoring': []}
                             if groups is not None else None)
     if pipe.param_routing:
@@ -2129,7 +2160,7 @@ pipe_config = {
             'estimator__class_weight': cv_params['svc_clf_cw'],
             'step': cv_params['rfe_clf_step'],
             'n_features_to_select': cv_params['skb_slr_k']},
-        'param_routing': ['sample_weight']},
+        'param_routing': ['feature_meta', 'sample_weight']},
     'RFE-SVC': {
         'estimator': RFE(svc_clf, tune_step_at=args.rfe_clf_tune_step_at,
                          reducing_step=args.rfe_clf_reducing_step,
@@ -2139,7 +2170,7 @@ pipe_config = {
             'estimator__class_weight': cv_params['svc_clf_cw'],
             'step': cv_params['rfe_clf_step'],
             'n_features_to_select': cv_params['skb_slr_k']},
-        'param_routing': ['sample_weight']},
+        'param_routing': ['feature_meta', 'sample_weight']},
     'RFE-RandomForestClassifier': {
         'estimator': RFE(rf_clf, tune_step_at=args.rfe_clf_tune_step_at,
                          reducing_step=args.rfe_clf_reducing_step,
@@ -2151,7 +2182,7 @@ pipe_config = {
             'estimator__class_weight': cv_params['rf_clf_cw'],
             'step': cv_params['rfe_clf_step'],
             'n_features_to_select': cv_params['skb_slr_k']},
-        'param_routing': ['sample_weight']},
+        'param_routing': ['feature_meta', 'sample_weight']},
     'RFE-ExtraTreesClassifier': {
         'estimator': RFE(ext_clf, tune_step_at=args.rfe_clf_tune_step_at,
                          reducing_step=args.rfe_clf_reducing_step,
@@ -2174,7 +2205,7 @@ pipe_config = {
             'estimator__max_features': cv_params['grb_clf_f'],
             'step': cv_params['rfe_clf_step'],
             'n_features_to_select': cv_params['skb_slr_k']},
-        'param_routing': ['sample_weight']},
+        'param_routing': ['feature_meta', 'sample_weight']},
     'LinearSVC': {
         'estimator': LinearSVC(
             loss=args.lsvc_clf_loss, max_iter=args.lsvc_clf_max_iter,

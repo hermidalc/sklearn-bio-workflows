fdbdfd381908f0c4635e5a8fc3a92ea0106b9e71
Leandro Hermida <hermidal@cs.umd.edu>
2021-06-25 11:02:45 -0400

    Update tumor stage ordinal encoder categories

--- select_model.py
+++ select_model.py
@@ -2612,7 +2612,7 @@ metric_label = {
     'average_precision': 'AVG PRE'}
 
 ordinal_encoder_categories = {
-    'tumor_stage': ['NA', 'x', 'i', 'i or ii', 'ii', 'iii', 'iv']}
+    'tumor_stage': ['0', 'i', 'i or ii', 'ii', 'NA', 'iii', 'iv']}
 
 run_model_selection()
 if args.show_figs or args.save_figs:
